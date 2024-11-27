fb9d736d4e7f73580799963485baa6ecf8eaeed4
hellonaeunkim <hellonaeunkim@gmail.com>
2024-11-27 15:29:23 +0900

    feat : 명언 목록 기능 추가

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -54,4 +54,28 @@ public class WiseSayingControllerTest {
         .contains("2번 명언이 등록되었습니다.")
         .contains("3번 명언이 등록되었습니다.");
   }
+
+  @Test
+  @DisplayName("목록 명령어 : 입력된 명언들을 출력한다.")
+  public void t7() {
+    String output = AppTest.run("""
+        등록
+        현재를 사랑하라.
+        작자미상
+        등록
+        과거에 집착하지 마라.
+        작자미상
+        등록
+        나의 죽음을 적들에게 알리지 말라!
+        이순신
+        목록
+        """);
+
+    assertThat(output)
+        .contains("번호 / 작가 / 명언")
+        .contains("------------------")
+         .contains("3 / 이순신 / 나의 죽음을 적들에게 알리지 말라!")
+        .contains("2 / 작자미상 / 과거에 집착하지 마라.")
+        .contains("1 / 작자미상 / 현재를 사랑하라.");
+  }
 }
