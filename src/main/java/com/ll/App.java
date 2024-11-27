4543591398e521012746c9eaa45f5d904d364182
hellonaeunkim <hellonaeunkim@gmail.com>
2024-11-27 10:24:37 +0900

    feat : 명언 명령 기능 추가

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,25 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class App {
+  private final Scanner scanner;
+
+  public App(Scanner scanner) {
+    this.scanner = scanner;
+  }
+
+  public void run() {
+    System.out.println("==명언 앱==");
+
+    while (true) {
+      System.out.println("명령) ");
+      String cmd = scanner.nextLine();
+
+      if("종료".equals(cmd)) {
+        break;
+      }
+    }
+  }
+}
+
---
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -0,0 +1,43 @@
+package com.ll.domain.wiseSaying.controller;
+
+import com.ll.AppTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WiseSayingControllerTest {
+  @Test
+  @DisplayName("==명언 앱==")
+  public void t1() {
+    String output = AppTest.run("종료");
+
+    assertThat(output)
+        .contains("==명언 앱==");
+  }
+
+  @Test
+  @DisplayName("명령) ")
+  public void t2() {
+    String output = AppTest.run("""
+        목록
+        종료
+        """);
+
+    assertThat(output)
+        .contains("명령) ");
+  }
+
+  @Test
+  @DisplayName("명령을 2번 이상 입력할 수 있습니다.")
+  public void t3() {
+    String output = AppTest.run("""
+        목록
+        목록
+        종료
+        """);
+
+    String[] split = output.split("명령\\)");
+    assertThat(split).hasSize(4);
+  }
+}
