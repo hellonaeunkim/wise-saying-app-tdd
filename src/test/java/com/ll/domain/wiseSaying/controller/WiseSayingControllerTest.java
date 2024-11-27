ffd03d20c53e856118250bb55776bf8707aa5044
hellonaeunkim <hellonaeunkim@gmail.com>
2024-11-27 12:34:00 +0900

    feat : 명언 등록 기능 추가

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -11,13 +11,26 @@ public class WiseSayingControllerTest {
   @DisplayName("등록을 입력하면 내용과 작가를 입력받는다.")
   public void t4() {
     String output = AppTest.run("""
-      등록
-      현재를 사랑하라.
-      작자미상
-      """);
+        등록
+        현재를 사랑하라.
+        작자미상
+        """);
 
     assertThat(output)
         .contains("명언 : ")
         .contains("작가 : ");
+  }
+
+    @Test
+    @DisplayName("등록이 완료되면 명언번호가 출력된다.")
+    public void t5() {
+      String output = AppTest.run("""
+        등록
+        현재를 사랑하라.
+        작자미상
+        """);
+
+      assertThat(output)
+          .contains("1번 명언이 등록되었습니다.");
   }
 }
