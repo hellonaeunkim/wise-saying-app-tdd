4690053ae4fc90f9f500702cb7da56fad7d88b24
hellonaeunkim <hellonaeunkim@gmail.com>
2024-12-03 17:14:10 +0900

    refactor : temp 폴더 생성/삭제 및 파일(폴더) 삭제 기능 개선

--- src/main/java/com/ll/standard/util/Util.java
+++ src/main/java/com/ll/standard/util/Util.java
@@ -1,10 +1,8 @@
 package com.ll.standard.util;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Util {
     public static class file {
@@ -13,7 +11,7 @@ public class Util {
         }
 
         public static boolean exists(String filePath) {
-            Path path = Path.get(filePath);
+            Path path = Paths.get(filePath);
             return Files.exists(path);
         }
 
@@ -23,7 +21,22 @@ public class Util {
             try {
                 Files.writeString(path, content, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
             } catch (IOException e) {
-                throw new RuntimeException(e);
+
+                final Path parentDir = path.getParent();
+                if (parentDir != null && Files.notExists(parentDir)) {
+                    try {
+                        Files.createDirectories(parentDir);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    try {
+                        Files.writeString(path, content, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                } else {
+                    throw new RuntimeException(e);
+                }
             }
         }
 
@@ -37,17 +50,48 @@ public class Util {
             }
         }
 
-        public static void delete(String filePath) {
-            Path path = Paths.get(filePath);
+        public static boolean delete(String filePath) {
+            final Path path = Paths.get(filePath);
+
             try {
-                Files.delete(path);
+                Files.walkFileTree(path, new SimpleFileVisitor<>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                        Files.delete(file); // 파일 삭제
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                        Files.delete(dir); // 디렉토리 삭제
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+                return true;
+            } catch (NoSuchFileException e) {
+                return false;
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                return false;
             }
         }
+
         public static boolean notExists(String filePath) {
             return !exists(filePath);
         }
 
+        public static void mkdir(String dirPath) {
+            Path path = Paths.get(dirPath);
+
+            try {
+                Files.createDirectories(path);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public static boolean rmdir(String dirPath) {
+            return delete(dirPath);
+        }
     }
 }
+
