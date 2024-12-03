a66c1688122eca1291c95226c2e4b57002d55085
hellonaeunkim <hellonaeunkim@gmail.com>
2024-12-03 16:27:46 +0900

    feat : CRUD test class 추가

---
+++ src/main/java/com/ll/standard/util/Util.java
@@ -0,0 +1,53 @@
+package com.ll.standard.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class Util {
+    public static class file {
+        public static void touch(String filePath) {
+            set(filePath, "");
+        }
+
+        public static boolean exists(String filePath) {
+            Path path = Path.get(filePath);
+            return Files.exists(path);
+        }
+
+        public static void set(String filePath, String content) {
+            Path path = Paths.get(filePath);
+
+            try {
+                Files.writeString(path, content, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public static String get(String filePath, String defaultValue) {
+            Path path = Paths.get(filePath);
+
+            try {
+                return Files.readString(path);
+            } catch (IOException e) {
+                return defaultValue;
+            }
+        }
+
+        public static void delete(String filePath) {
+            Path path = Paths.get(filePath);
+            try {
+                Files.delete(path);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        public static boolean notExists(String filePath) {
+            return !exists(filePath);
+        }
+
+    }
+}
---
+++ src/test/java/com/ll/standard/util/UtilTest.java
@@ -0,0 +1,48 @@
+package com.ll.standard.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UtilTest {
+    @Test
+    @DisplayName("파일을 생성할 수 있다.")
+    public void t1() {
+        String filePath = "temp/test.txt";
+
+        Util.file.touch(filePath);
+
+        assertThat(
+                Util.file.exists(filePath)
+        ).isTrue();
+
+        Util.file.delete(filePath);
+    }
+
+    @Test
+    @DisplayName("파일의 내용을 수정할 수 있고, 읽을 수 있다.")
+    public void t2() {
+        String filePath = "temp/test.txt";
+
+        Util.file.set(filePath, "내용");
+
+        assertThat(
+                Util.file.get(filePath, "")
+        ).isEqualTo("내용");
+        Util.file.delete(filePath);
+    }
+
+    @Test
+    @DisplayName("파일을 삭제할 수 있다.")
+    public void t3 () {
+        String filePath = "temp/test.txt";
+
+        Util.file.touch(filePath);
+        Util.file.delete(filePath);
+
+        assertThat(
+                Util.file.notExists(filePath)
+        ).isTrue();
+    }
+}
