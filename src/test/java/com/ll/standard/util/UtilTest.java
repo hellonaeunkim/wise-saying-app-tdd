bdd74112486506cdfe93113bf221f391be90258a
hellonaeunkim <hellonaeunkim@gmail.com>
2024-12-03 17:35:50 +0900

    feat : 파일 관련 Util 함수 강화 (파일을 생성할 때 디렉토리가 없다면 디렉토리부터 만든다.)

--- src/test/java/com/ll/standard/util/UtilTest.java
+++ src/test/java/com/ll/standard/util/UtilTest.java
@@ -57,4 +57,17 @@ public class UtilTest {
                 Util.file.notExists(filePath)
         ).isTrue();
     }
+
+    @Test
+    @DisplayName("파일을 생성할 수 있다, 만약 경로의 폴더가 없다면 만든다.")
+    public void t4 () {
+        String filePath = "temp/temp/test.txt";
+
+        Util.file.touch(filePath);
+        Util.file.delete(filePath);
+
+        assertThat(
+                Util.file.notExists(filePath)
+        ).isTrue();
+    }
 }
