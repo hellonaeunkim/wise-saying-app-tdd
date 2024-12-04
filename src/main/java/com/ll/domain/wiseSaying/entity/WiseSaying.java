7d68fea395cee2db328d8fb2e1bb689aa8e3e443
hellonaeunkim <hellonaeunkim@gmail.com>
2024-12-04 23:58:33 +0900

    refactor : Lombok 추가로 코드 간결화

--- src/main/java/com/ll/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/ll/domain/wiseSaying/entity/WiseSaying.java
@@ -1,39 +1,16 @@
 package com.ll.domain.wiseSaying.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class WiseSaying {
   private int id;
   private String content;
   private String author;
-
-  public WiseSaying(int id, String content, String author) {
-    this.id = id;
-    this.content = content;
-    this.author = author;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public String getAuthor() {
-    return author;
-  }
-
-  public String getContent() {
-    return content;
-  }
-
-  public void setContent(String content) {
-    this.content = content;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public void setAuthor(String author) {
-    this.author = author;
-  }
 
   public boolean isNew() {
     return id == 0;
