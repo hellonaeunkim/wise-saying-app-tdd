e3754463aedfef13b9c6b0d969e5874a8b31066a
hellonaeunkim <hellonaeunkim@gmail.com>
2024-12-06 15:40:51 +0900

    refactor : WiseSayingMemoryRepository & WiseSayingFileRepository 생성

--- src/main/java/com/ll/domain/wiseSaying/repository/WiseSayingRepository.java
+++ src/main/java/com/ll/domain/wiseSaying/repository/WiseSayingRepository.java
@@ -2,44 +2,16 @@ package com.ll.domain.wiseSaying.repository;
 
 import com.ll.domain.wiseSaying.entity.WiseSaying;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class WiseSayingRepository {
+public interface WiseSayingRepository {
 
-  private int lastId;
-  private final List<WiseSaying> wiseSayings;
+  WiseSaying save(WiseSaying wiseSaying);
 
-  public WiseSayingRepository() {
-    this.lastId = 0;
-    this.wiseSayings = new ArrayList<>();
-  }
+  List<WiseSaying> findAll();
 
+  boolean deleteById(int id);
 
-  public WiseSaying save(WiseSaying wiseSaying) {
-    // 메모리 저장 특성상 새 객체가 아니라면 딱히 할게 없다.
-    if (!wiseSaying.isNew()) {
-      return wiseSaying;
-    }
-
-    // 새로운 객체라면 id를 할당하고 리스트(wiseSayings)에 추가
-    wiseSaying.setId(++lastId);
-    wiseSayings.add(wiseSaying);
-    return wiseSaying;
-  }
-
-  public List<WiseSaying> findAll() {
-    return wiseSayings;
-  }
-
-  public boolean deleteById(int id) {
-    return wiseSayings.removeIf(wiseSaying -> wiseSaying.getId() == id);
-  }
-
-    public Optional<WiseSaying> findById(int id) {
-      return wiseSayings.stream()
-          .filter(wiseSaying -> wiseSaying.getId() == id)
-          .findFirst();
-    }
+  Optional<WiseSaying> findById(int id);
 }
